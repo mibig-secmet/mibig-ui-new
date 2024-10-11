34b28986c1c1d2ce4863ea392ffd5be0cc340d0c
Kai Blin <kblin@biosustain.dtu.dk>
2024-10-11 11:56:32 +0200

    stats: Make the stats less confusing
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- src/pages/stats.vue
+++ src/pages/stats.vue
@@ -29,11 +29,12 @@ export default {
           single: true,
           highlight: true,
         },
-        { description: "Complete entries", count: 0 },
-        { description: "Partial entries", count: 0 },
+        { description: "Active entries", count: 0, single: true },
         { description: "Pending entries", count: 0 },
         { description: "Retired entries", count: 0 },
-        { description: "Active entries", count: 0, single: true },
+        { description: "Complete entries", count: 0, single: true },
+        { description: "Entries with unknown completion", count: 0 },
+        { description: "Partial entries", count: 0 },
       ],
       record_count: [
         { description: "Loading...", count: 0 },
@@ -46,11 +47,12 @@ export default {
       async handler() {
         const data = await this.fetchStats();
         this.general_stats[0].count = data.counts.total;
-        this.general_stats[1].count = data.counts.complete;
-        this.general_stats[2].count = data.counts.partial;
-        this.general_stats[3].count = data.counts.pending;
-        this.general_stats[4].count = data.counts.retired;
-        this.general_stats[5].count = data.counts.active;
+        this.general_stats[1].count = data.counts.active;
+        this.general_stats[2].count = data.counts.pending;
+        this.general_stats[3].count = data.counts.retired;
+        this.general_stats[4].count = data.counts.complete;
+        this.general_stats[5].count = data.counts.total - data.counts.complete - data.counts.partial;
+        this.general_stats[6].count = data.counts.partial;
 
         this.record_count = data.clusters;
 
